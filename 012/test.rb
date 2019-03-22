b0467bea1f148b7208b57e15622cea7a9a3e5c7e
Gerald Bauer <gerald.bauer@gmail.com>
2019-03-22 21:49:58 +0100

    Update test.rb

--- 012/test.rb
+++ 012/test.rb
@@ -19,19 +19,19 @@ pp tictactoe
 Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:create, '0xbbbb', '0xaaaa'])
 pp tictactoe
 
-# 1. move by 0xaaaaa
+# 1st move by 0xaaaaa
 Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 0, 0])
 pp tictactoe
-# 1. move by 0xbbbb
+# 1st move by 0xbbbb
 Uni.send_transaction( from: '0xbbbb', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xbbbb', 0, 1])
 pp tictactoe
-# 2.move by 0xaaaa
+# 2nd move by 0xaaaa
 Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 1, 1])
 pp tictactoe
-# 2. move by 0xbbbb
+# 2nd move by 0xbbbb
 Uni.send_transaction( from: '0xbbbb', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xbbbb', 0, 2])
 pp tictactoe
-# 3.move by 0xaaaa  - win!
+# 3rd move by 0xaaaa - WIN!
 Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 2, 2])
 pp tictactoe
 
