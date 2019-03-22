cd4cdf4e6550e1426684218fa5964bdfcf8b599c
Gerald Bauer <gerald.bauer@gmail.com>
2019-03-22 21:48:27 +0100

    Create test.rb

---
+++ 012/test.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+require 'universum'
+
+
+TicTacToe = Contract.load( './tictactoe' )
+pp TicTacToe
+
+####
+# setup test accounts
+Account[ '0xaaaa' ]
+Account[ '0xbbbb' ]
+
+
+tx = Uni.send_transaction( from: '0xaaaa', data: [TicTacToe] )
+tictactoe = tx.receipt.contract
+pp tictactoe
+
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:create, '0xbbbb', '0xaaaa'])
+pp tictactoe
+
+# 1. move by 0xaaaaa
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 0, 0])
+pp tictactoe
+# 1. move by 0xbbbb
+Uni.send_transaction( from: '0xbbbb', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xbbbb', 0, 1])
+pp tictactoe
+# 2.move by 0xaaaa
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 1, 1])
+pp tictactoe
+# 2. move by 0xbbbb
+Uni.send_transaction( from: '0xbbbb', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xbbbb', 0, 2])
+pp tictactoe
+# 3.move by 0xaaaa  - win!
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:move, '0xbbbb', '0xaaaa', '0xaaaa', 2, 2])
+pp tictactoe
+
+
+###
+# try restart
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:restart, '0xbbbb', '0xaaaa', '0xaaaa'])
+pp tictactoe
+
+###
+# try cleanup
+Uni.send_transaction( from: '0xaaaa', to: tictactoe, data: [:close, '0xbbbb', '0xaaaa'])
+pp tictactoe
+
+##
+# try another game
+Uni.send_transaction( from: '0xbbbb', to: tictactoe, data: [:create, '0xaaaa', '0xbbbb'])
+pp tictactoe
